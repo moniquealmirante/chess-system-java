eb052d56b08feaedda4048f5c65fa3a3395f50eb
Monique Almirante <monique.kinha@gmail.com>
2023-09-20 04:29:19 -0300

    First class: Position

--- src/application/Pogram.java
+++ src/application/Pogram.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Pogram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("olálupin");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
