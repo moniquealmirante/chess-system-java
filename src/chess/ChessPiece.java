98209cc9d479aff43cb0536ac73bede451e8b77b
Monique Almirante <monique.kinha@gmail.com>
2023-09-21 02:24:21 -0300

    Placing pieces on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,18 +4,20 @@ import boardgame.Board;
 import boardgame.Piece;
 
 public class ChessPiece extends Piece {
-
+	
 	private Color color;
-	
-	
-	public ChessPiece(Board board) {
+
+	public ChessPiece(Board board, Color color) {
 		super(board);
-		
+		this.color = color;
 	}
 
 	public Color getColor() {
 		return color;
 	}
+
+
+	
 
 
 
